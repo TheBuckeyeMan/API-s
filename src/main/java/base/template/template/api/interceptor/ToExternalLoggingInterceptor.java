08e4f4e58b58ef909ba9a99697473e292f0fa6d4
Adam Maas <adamjm1220@gmail.com>
2024-08-29 11:10:17 -0400

    Loggs added for to and from requests

--- src/main/java/base/template/template/api/interceptor/ToExternalLoggingInterceptor.java
+++ src/main/java/base/template/template/api/interceptor/ToExternalLoggingInterceptor.java
@@ -1,46 +1,33 @@
 package base.template.template.api.interceptor;
 
 import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
-import java.io.OutputStream;
 
-
-
+//The entire purpose of this to external logging interceptor is to log our request we are making to the external data source for debugging purposes.
 
 @Component
 public class ToExternalLoggingInterceptor implements ClientHttpRequestInterceptor {
     
     private static final Logger log = LoggerFactory.getLogger(ToExternalLoggingInterceptor.class);
-    private final ObjectMapper objectMapper = new ObjectMapper(); //Object for JSON Request
     
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        logRequestDetails(request, body);//Log Request of Details
+        logRequestDetails(request, body);//Log Details of request
         return execution.execute(request, body); //proceed with request
     }
 
     private void logRequestDetails(HttpRequest request, byte[] body) throws IOException {
         String requestBody = new String(body, "UTF-8"); //Convert request body to json format
-
         //Log the Request Details
-        log.info("Request URL: {}", request.getURI());
-        log.info("Request Method: {}", request.getMethod());
-        log.info("Request Headers: {}", request.getHeaders()); //Authentication
-        log.info("Request Body: {}", requestBody);
+        log.info("Outbound Request URL: {}", request.getURI());
+        log.info("Outbound Request Method: {}", request.getMethod());
+        log.info("Outbound Request Headers: {}", request.getHeaders()); //Authentication
+        log.info("Outbound Request Body: {}", requestBody);
     }
-
-
-
-
-
-
 }
